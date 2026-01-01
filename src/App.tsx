8a5a844a15d0066603412ed3a7989366aaa6d5b3
LuckyThanet <thanet.jomp@kmutt.ac.th>
2026-01-01 10:53:08 +0700

    feat: add Layout component and integrate AppNavbar for improved routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 // src/App.tsx หรือ Router configuration
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import { CallbackPage } from "./pages/CallbackPage";
 import HomePage from "./pages/HomePage";
 import { MsalAuthenticationTemplate } from "@azure/msal-react";
-
+import AppNavbar from "./components/common/AppNavbar";
 import { InteractionType } from "@azure/msal-browser";
 
+
+function Layout() {
+  return (
+    <div>
+      <AppNavbar />
+      <Outlet />
+    </div>
+  );
+}
+
+
 function App() {
-  console.log("test" , import.meta.env.VITE_ADFS_CLIENTID);
+  // console.log("test" , import.meta.env.VITE_ADFS_CLIENTID);
   return (
     <Routes>
-
-      {/* แนะนำสร้าง Applayout ขึ้นมานะครับ
+      <Route element={<Layout />}>
+        {/* แนะนำสร้าง Applayout ขึ้นมานะครับ
        <Route element={
         <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
           <AppLayout/>
@@ -20,16 +31,19 @@ function App() {
 
       </Route>
        */}
-      <Route
-        path="/"
-        element={
-          <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-            <HomePage />
-          </MsalAuthenticationTemplate>
-        }
-      />
+        <Route
+          path="/"
+          element={
+            <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
+              <HomePage />
+            </MsalAuthenticationTemplate>
+          }
+        />
+      </Route>
 
-      <Route path="/auth/callback" element={<CallbackPage />} />
+      {/* <Route path="/auth/callback" element={<CallbackPage />} /> */}
+      <Route path="/redirect" element={<CallbackPage />} />
+      <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
   );
 }
